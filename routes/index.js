a64a4e73fa870b58cdea6b9ce8ee392da5e3b87d
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-10-29 21:18:25 -0400

    landing and non-logged-in index page created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index')
 });
+
+router.get('/home', (req, res) => {
+  res.render('home/home')
+})
 
 module.exports = router;
