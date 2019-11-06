65becf258be9eaed98c664b2fe7bffc00b88f2a4
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-11-06 12:58:39 -0500

    test

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URI,
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, ['views', 'gameFiles']));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -40,6 +40,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'gameFiles')));
 app.use(methodOverride('_method'));
 
 app.use(session({
--- routes/game/controllers/launchController.js
+++ routes/game/controllers/launchController.js
@@ -1,11 +1,5 @@
-const express = require('express')
-const path = require('path');
-
 module.exports = {
     gameLaunch: (req, res) => {
-        let app = express();
-        app.use(express.static(path.join(__dirname, 'gameFiles')));
-        app.set('views', path.join(__dirname, 'gameFiles'));
         res.render('index')
     }
 
