6f3779a523036c158f38e5f15ffd449688cbb8f8
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-11-06 12:48:21 -0500

    test

--- routes/game/controllers/launchController.js
+++ routes/game/controllers/launchController.js
@@ -5,7 +5,7 @@ module.exports = {
     gameLaunch: (req, res) => {
         let app = express();
         app.use(express.static(path.join(__dirname, 'gameFiles')));
-        res.render('../gameFiles/index')
+        res.render(path.join('../', 'gameFiles/index'))
     }
 
 }
